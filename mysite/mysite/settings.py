bfcefc83ee3fdf1055faed9e2b375988fbf2f74b
Shred <daerrey3@gmail.com>
2025-08-30 22:39:31 +1100

    add test

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,18 +77,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    # "default": {
-    #     "ENGINE": "django.db.backends.postgresql",
-    #     "NAME": "test",
-    #     "USER": "admin",
-    #     "PASSWORD": "admin",
-    #     "HOST": "postgres",
-    #     "PORT": "5432",
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "test",
+        "USER": "admin",
+        "PASSWORD": "admin",
+        "HOST": "postgres",
+        "PORT": "5432",
+    }
 }
 
 
